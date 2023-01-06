cc443ad642b070895eb3646d8415c91700b98b4f
Bilal Malik <bmeta2121@gmail.com>
2023-01-05 19:52:39 -0500

    Spring Boot Demo

---
+++ SpringBootDemo/src/main/java/com/sookocheff/springdemo/Player.java
@@ -0,0 +1,29 @@
+package com.sookocheff.springdemo;
+
+public class Player {
+
+    private final String playerId;
+    private final String alias;
+    private final String displayName;
+
+    public Player(final String id,
+                  final String alias,
+                  final String displayName) {
+        this.playerId = id;
+        this.alias = alias;
+        this.displayName = displayName;
+    }
+
+    public String getPlayerId() {
+        return playerId;
+    }
+
+    public String getAlias() {
+        return alias;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
+}
+
